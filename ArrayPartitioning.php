9c0fe0aef296a2920f2ad364a782dd1efe44c49d
Dmitry Patsura <zaets28rus@gmail.com>
2016-04-03 22:46:07 +0300

    Initial part / Letst think about best approach

---
+++ ArrayPartitioning.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+class ArrayPartitioning
+{
+    /**
+     * @param array|int[] $inputNumbers Input array of the numbers
+     * @param int $numArrays How many arrays you need to get from $inputNumbers
+     * @return array|int[]
+     */
+    public function algoritmOne(array $inputNumbers, int $numArrays)
+    {
+        if ($numArrays <= 0) {
+            throw new InvalidArgumentException('$numArrays must be a positive number');
+        }
+
+        $outputArrays = []; ////  [[8,2], [7,2,1], [6,4,1]]
+
+        return $outputArrays;
+    }
+}
